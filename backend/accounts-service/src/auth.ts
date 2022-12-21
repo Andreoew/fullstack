1f03c7ca80ee70a6dde69efcdd7b41f53c71ccdb
André de Souza <andreoew@hotmail.com>
2022-12-21 19:32:07 -0300

    update code bcrypt

--- backend/accounts-service/src/auth.ts
+++ backend/accounts-service/src/auth.ts
@@ -1,4 +1,16 @@
 import bcrypt from 'bcryptjs';
+import jwt, { VerifyOptions } from 'jsonwebtoken';
+import fs from 'fs';
+import path from 'path';
+
+//const privates = path.dirname('.src/keys/private.key')
+//console.log(privates)
+
+const privatekey = fs.readFileSync('./src/keys/private.key', 'utf8');
+const publickey = fs.readFileSync('./src/keys/public.key', 'utf8');
+const jwtExpires = parseInt(`${process.env.JWT_EXPIRES}`);
+const jwtAlgorithm = "RS256";
+
 
 function hashPassword(password: string){
     return bcrypt.hashSync(password, 10);
@@ -8,4 +20,21 @@ function comparePassword(password: string, hashPassword: string){
   return bcrypt.compare(password, hashPassword);
 }
 
-export default { hashPassword, comparePassword };
+type Token = { accountId: number };
+
+function sign(accountId: number){
+  const token: Token = {accountId}
+  return jwt.sign(token, privatekey, { expiresIn: jwtExpires, algorithm: jwtAlgorithm })
+}
+
+async function verify(token: string){
+  try {
+    const decoded: Token = await jwt.verify(token, publickey, {algorithm: [jwtAlgorithm]} as VerifyOptions) as Token;
+    return { accountId: decoded.accountId };
+  } catch (error) {
+    console.log(`verify: ${error}`);
+    return null;
+  }
+}
+
+export default { hashPassword, comparePassword, sign, verify };
--- backend/accounts-service/src/controllers/accounts.ts
+++ backend/accounts-service/src/controllers/accounts.ts
@@ -1,28 +1,30 @@
 import { Request, Response } from "express";
 import auth from "../auth";
 import { IAccount } from "../models/account";
-import  repository from '../models/accountModel'
+import repository from "../models/accountModel";
 
 const accounts: IAccount[] = [];
 
 async function getAccounts(req: Request, res: Response, next: any) {
   const accounts = await repository.findAll();
-  res.json(accounts.map(item => {
-    item.password = '';
-    return item;
-  }));
+  res.json(
+    accounts.map((item) => {
+      item.password = "";
+      return item;
+    })
+  );
 }
 
 async function getAccount(req: Request, res: Response, next: any) {
   try {
     const id = parseInt(req.params.id);
-    if(!id) throw new Error("ID is invalid format");
-    
+    if (!id) throw new Error("ID is invalid format");
+
     const account = await repository.findById(id);
     if (account === null) {
       return res.status(404).end();
     } else {
-      account.password = '';
+      account.password = "";
       res.json(account);
     }
   } catch (error) {
@@ -36,7 +38,7 @@ async function addAccount(req: Request, res: Response, next: any) {
     const newAccount = req.body as IAccount;
     newAccount.password = auth.hashPassword(newAccount.password);
     const result = await repository.add(newAccount);
-    newAccount.password = '';
+    newAccount.password = "";
     newAccount.id = result.id;
     res.status(201).json(newAccount);
   } catch (error) {
@@ -45,15 +47,15 @@ async function addAccount(req: Request, res: Response, next: any) {
   }
 }
 
-async function setAccount(req: Request, res: Response, next: any){
+async function setAccount(req: Request, res: Response, next: any) {
   try {
     const accountId = parseInt(req.params.id);
-    if(!accountId) throw new Error('ID is in invalid format');
+    if (!accountId) throw new Error("ID is in invalid format");
 
     const accountParams = req.body as IAccount;
     accountParams.password = auth.hashPassword(accountParams.password);
     const updatedAccount = await repository.set(accountId, accountParams);
-    updatedAccount.password = '';
+    updatedAccount.password = "";
     res.status(200).json(updatedAccount);
   } catch (error) {
     console.log(error);
@@ -61,16 +63,33 @@ async function setAccount(req: Request, res: Response, next: any){
   }
 }
 
-function loginAccount(req: Request, res: Response, next: any){
-  const loginParams = req.body as IAccount;
-  const index = accounts.findIndex(item => item.email === loginParams.email && item.password === loginParams.password);
-  if(index === -1) return res.status(401).end();
-
-  res.json({ auth: true, token: {}});
+async function loginAccount(req: Request, res: Response, next: any) {
+  try {
+    const loginParams = req.body as IAccount;
+    const account = await repository.findByEmail(loginParams.email);
+    if (account !== null) {
+      const isValid = auth.comparePassword(
+        loginParams.password,
+        account.password
+      );
+      if (await isValid) {
+        const token = await auth.sign(account.id!);
+        res.json({ auth: true, token });
+      }
+    }
+    return res.status(401).end();
+  } catch (error) {}
 }
 
-function logaoutAccount(req: Request, res: Response, next: any){
-  res.json({auth: false, token: null})
+function logaoutAccount(req: Request, res: Response, next: any) {
+  res.json({ auth: false, token: null });
 }
 
-export default { getAccount, getAccounts, addAccount, setAccount, loginAccount, logaoutAccount };
+export default {
+  getAccount,
+  getAccounts,
+  addAccount,
+  setAccount,
+  loginAccount,
+  logaoutAccount,
+};
